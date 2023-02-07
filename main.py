a449de3f65f6d38b875d5e5797d41f6e4393387e
Daniel <danielWong151999@gmail.com>
2023-02-07 12:33:57 +0800

    Add 3 more operations

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 emp_list = []
 
-while(True):
-    first_num = input("Enter number: ")
+def addition(x, y):
+    total = x + y
+    emp_list.append(total)
 
-    second_num = input("Enter number: ")
+def substration(x, y):
+    total = x - y
+    emp_list.append(total)
 
-    emp_list.append(float(first_num))
-    emp_list.append(float(second_num))
+def multiply(x, y):
+    total = x * y
+    emp_list.append(total)
 
-    total = sum(emp_list)
-    
-    print(total)
+def divide(x, y):
+    total = x / y
+    emp_list.append(total)
 
-    get_input = input("Enter 'n' to exit: ")
+first_num = float(input("Enter number: "))
 
-    if get_input == 'n':
-        break
-    else:
-        third_num = input("Enter number: ")
+operation_choice = input("What operation: ")
 
-        emp_list.append(float(third_num))
+if operation_choice == "add":
+    second_num = float(input("Enter second number: "))
 
-        total = sum(emp_list)
-        
-        print(total)
-        
-        break
+    addition(first_num, second_num)
+elif operation_choice == "minus":
+    second_num = float(input("Enter second number: "))
+
+    substration(first_num, second_num)
+elif operation_choice == "times":
+    second_num = float(input("Enter second number: "))
+
+    multiply(first_num, second_num)
+elif operation_choice == "divide":
+    second_num = float(input("Enter second number: "))
+
+    divide(first_num, second_num)
+else:
+    print(emp_list)
+
+get_total = sum(emp_list)
+
+continue_choice = input("Enter n to stop: ")
+
+if continue_choice == 'n':
+
+    print(get_total)
+
+else:
+    while(True):
+
+        third_number = float(input("Enter number: "))
+
+        get_total = get_total + third_number
+
+        print(get_total)
